14c6774c2b916ad6480fad4267aa912d8ef2aaaf
psz <psz@bs.com>
2022-06-07 00:48:21 +0200

    Stripping in inputs

--- 42makefile-maker.py
+++ 42makefile-maker.py
@@ -40,7 +40,7 @@ makefile_contents = f'''# ******************************************************
 # **************************************************************************** #'''
 
 project_is_libft = False
-project_is_libft = input("Is this project the libft itself? (yes/no)\n")
+project_is_libft = input("Is this project the libft itself? (yes/no)\n").strip()
 executables_names = None
 
 if utils.is_positive_response(project_is_libft):
@@ -52,11 +52,11 @@ else:
 
 if  not project_is_libft:
     libft_path = "./libft"
-    libft_present = input("Have you included your libft in your project? (yes/no)\n")
+    libft_present = input("Have you included your libft in your project? (yes/no)\n").strip()
     if utils.is_positive_response(libft_present):
         libft_present = True
         while True:
-            libft_path = input("Input the name of the libft folder (leave empty for \"libft\". If you haven't included your libft, type nolibft\n")
+            libft_path = input("Input the name of the libft folder (leave empty for \"libft\". If you haven't included your libft, type nolibft\n").strip()
             if os.path.exists(libft_path) or libft_path == "nolibft":
                 if libft_path == "nolibft":
                     libft_present = False
@@ -79,6 +79,6 @@ else:
 
 executables_names = None
 print("Input the name(s) of executables/archives/outfiles you must generate. If there's more than one, separate the names by using a semicolon \";\"")
-names = input("Example 1: libft.a\nExample 2: client;server")
+names = input("Example 1: libft.a\nExample 2: client;server").strip()
 
 utils.save_makefile_prompt_make(makefile_contents)
